88ab32550b0395cc7446a467b986599b2cd15819
jererele <jeree.castroo10@gmail.com>
2026-06-11 13:47:25 -0300

    fix: add connect-src and wss to CSP to allow Appwrite Realtime WebSockets

--- next.config.js
+++ next.config.js
@@ -32,7 +32,7 @@ const nextConfig = {
             },
             {
               key: 'Content-Security-Policy',
-              value: "default-src 'self' https://cloud.appwrite.io https://*.firebaseio.com https://*.googleapis.com https://*.firebaseapp.com https://*.firebase.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://apis.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://lh3.googleusercontent.com https://*.google.com https://*.firebaseapp.com; frame-src 'self' https://*.firebaseapp.com https://*.firebaseio.com;",
+              value: "default-src 'self' https://cloud.appwrite.io https://*.firebaseio.com https://*.googleapis.com https://*.firebaseapp.com https://*.firebase.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://apis.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://lh3.googleusercontent.com https://*.google.com https://*.firebaseapp.com; frame-src 'self' https://*.firebaseapp.com https://*.firebaseio.com; connect-src 'self' https://cloud.appwrite.io wss://cloud.appwrite.io https://*.googleapis.com https://*.firebaseio.com;",
             },
           ],
         },
